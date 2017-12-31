f5cebf293244f4859db8486ef681e4fc5bf5cce3
g0d <pltwmail@gmail.com>
2017-12-31 20:23:31 +0100

    Including for, range, break

--- flow.py
+++ flow.py
@@ -1,5 +1,6 @@
-x = int(input("Number: "))
-
+print('if statement')
+# x = int(input("Number: "))
+x = 0
 if x > 0:
     print("Positive")
 
@@ -8,3 +9,36 @@ elif x == 0:
 
 else:
     print("Negative")
+
+''' loops '''
+print('\n loops \n')
+
+# Iterable list
+words = ['one', 'two', 'three']
+
+for i in words:
+    print(i)
+
+''' range '''
+
+print("\n range() \n")
+
+for i in range(5):
+    print(i)
+
+'''range() returns a referenceable object rather than a list'''
+l = range(5)
+
+print(list(range(5)))
+print(l[0])
+
+''' break in for '''
+print('break in for loop\n')
+
+for i in range(5):
+    for k in range(2, 5):
+        if i % k == 0:
+            print("break: ", i, "%", k, " == 0")
+            break
+    else:
+        print("break:  not occurred")
