dd80de76e81defcca8aebce8ca3360d7d278ce52
g0d <pltwmail@gmail.com>
2018-01-04 00:18:19 +0100

    More on fnc defs

--- functions.py
+++ functions.py
@@ -18,3 +18,47 @@ def fib(n: int):
 print("Evaluating")
 fib(2000)
 
+
+def fib2(n: int):
+    """
+
+    :param n:
+    :return: list
+    """
+
+    a, b = 0, 1
+    num = []
+
+    while a < n:
+        num.append(a)
+        a, b, = b, a + b
+
+    return num
+
+
+ret = fib2(2000)
+print(ret)
+
+i = 5
+
+
+def f1(n=i):
+    print('\n', n)
+
+
+i = 6
+f1()
+
+
+def f2(a, tab=None):
+    if tab == None:
+        tab = []
+    tab.append(a)
+    print(tab)
+
+
+f2(1)
+f2(2)
+f2(3)
+
+f2(6, [4, 5])
